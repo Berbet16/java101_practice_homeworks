26b7afbe2382998e1e80d568c1a0e0e1b047a587
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-03 21:53:57 +0300

    Update Vücut_Kitle_Indeksi_Hesaplayan_Program.java

--- Vücut_Kitle_Indeksi_Hesaplayan_Program.java
+++ Vücut_Kitle_Indeksi_Hesaplayan_Program.java
@@ -1,1 +1,22 @@
 
+import java.util.Scanner;
+
+public class kilo_endeks {
+    public kilo_endeks() {
+    }
+
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Lütfen Boyunuzu(metre cinsinden) Giriniz :");
+        double boy = input.nextDouble();
+        
+        System.out.println("Lütfen Kilonuzu Giriniz :");
+        double kilo = input.nextDouble();
+        
+        double endeks = kilo / Math.pow(boy / 100.0D, 2.0D);
+        System.out.println("\nEndeksiniz = " + endeks);
+
+    }
+}

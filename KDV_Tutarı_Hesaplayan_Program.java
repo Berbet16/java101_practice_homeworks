4c9d350d082338bc59f377b3dab6e6ca19539d37
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-03 21:17:12 +0300

    Update KDV_Tutarı_Hesaplayan_Program.java

--- KDV_Tutarı_Hesaplayan_Program.java
+++ KDV_Tutarı_Hesaplayan_Program.java
@@ -1,1 +1,25 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("KDV'siz Fiyat: ");
+        float kdvsiz = scan.nextFloat();
+        
+        double kdv_dahil;
+        
+        if(kdvsiz >0 && kdvsiz <= 1000)
+        {
+        	kdv_dahil =(kdvsiz*1.18);
+        }
+        else
+        {
+        	kdv_dahil = (kdvsiz*1.08);
+        }
+        
+        System.out.println("ÜrününKDV Fiyatı:" + kdv_dahil);
+    }
+}

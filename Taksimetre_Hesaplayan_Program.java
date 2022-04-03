2f9426776f1470bfa2e6e06c2c5aa1cd0b720d56
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-03 21:35:24 +0300

    Update Taksimetre_Hesaplayan_Program.java

--- Taksimetre_Hesaplayan_Program.java
+++ Taksimetre_Hesaplayan_Program.java
@@ -1,1 +1,22 @@
 
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       double km,ucret;
+
+       System.out.println("Gidilen kilometreyi yazınız : ");
+       km = input.nextFloat();
+       ucret = (km*2.20)+10.0;
+
+       if (ucret <= 20){
+           System.out.println("Orjinal Tutar = " + ucret + "TL");
+           System.out.println("Ödenecek Tutar = " + 20.0 + "TL");
+        }else{
+           System.out.println("Ödenecek ücret = " + ucret + "TL");
+       }
+
+
+    }
+}

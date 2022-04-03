d4e780afdd7c0057890f08f27e0d88319d759553
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-03 21:42:10 +0300

    Update Dairenin_Alanını_ve_Çevresini_Bulan_Program.java

--- Dairenin_Alanını_ve_Çevresini_Bulan_Program.java
+++ Dairenin_Alanını_ve_Çevresini_Bulan_Program.java
@@ -1,1 +1,23 @@
 
+import java.util.Scanner;
+
+public class daire_hesaplama {
+    public static void main(String[] args) {
+        float r,a;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Yarı Çapı giriniz");
+        r = input.nextFloat();
+
+        System.out.println("Açıyı giriniz");
+        a = input.nextFloat();
+
+        double cevre = 2*r* Math.PI ;
+        double alan = Math.PI * r*r;
+        double tarali_alan = ((alan*a)/360);
+
+        System.out.println("Çevre = " + cevre + " Alan = " + alan + " Taralı Alan = " + tarali_alan);
+
+    }
+}

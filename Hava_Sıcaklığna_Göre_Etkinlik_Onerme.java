926c6ac5fde44799123b3a1a976c9434b08d0f2a
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-04 14:05:49 +0300

    Create Hava_Sıcaklığna_Göre_Etkinlik_Onerme.java

---
+++ Hava_Sıcaklığna_Göre_Etkinlik_Onerme.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      int hot;
+
+      Scanner input = new Scanner(System.in);
+
+      System.out.print("Enter Temperature: ");
+      hot = input.nextInt();
+
+      if ((hot >= 0) && (hot < 5)) 
+      {
+          System.out.println("You can ski.");
+      } 
+      else if ((hot >= 5) && (hot <= 25)) 
+      {
+          if (hot <= 15) 
+          {
+            System.out.println("Yoou can go cinema.");
+          } 
+          else 
+          {
+              System.out.println("You can go picnic.");
+          }
+      } 
+      else 
+      {
+          System.out.println("Yüzmeye Gidebilirsiniz.");
+      }
+    }
+}

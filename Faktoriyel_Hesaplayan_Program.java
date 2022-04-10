5436f3c8f62e5ca243aa2918d69ca56677af4d1c
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-10 22:23:23 +0300

    Create Faktoriyel_Hesaplayan_Program.java

---
+++ Faktoriyel_Hesaplayan_Program.java
@@ -0,0 +1,22 @@
+package Odev;
+
+import java.util.Scanner;
+
+public class PatikaOdev 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	     Scanner scan = new Scanner(System.in);
+	     System.out.print("Faktoriyel Sayısı: ");
+	     int n = scan.nextInt();
+	     int total = 1;
+	     
+	     for(int i = 1; i <= n; i++)
+	     {
+	    	 total *= i;
+	     }
+	     System.out.println(n + " sayısının Faktöriyel Sayısının açılımı:" + total);
+	}
+}

539ea69a3aced7eec01419e64927beb0da28cf42
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-10 22:46:27 +0300

    Create Armstrong_Sayısı_Bulma.java

---
+++ Armstrong_Sayısı_Bulma.java
@@ -0,0 +1,26 @@
+package Odev;
+import java.util.Scanner;
+
+public class PatikaOdev 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int a,toplam=0;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz");
+        a=inp.nextInt();
+
+        while (a > 0)
+        {
+            System.out.println(a % 10);
+            toplam=( a % 10 ) + toplam;
+            a = a / 10;
+
+        }
+        System.out.print(toplam);
+	}
+}
